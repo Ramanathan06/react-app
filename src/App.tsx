d545620e423db7dd0a9e3aaded841f965e2fcfd9
Ramanathan M <ramanathan06ai@gmail.com>
2026-07-19 22:20:01 +0530

    Fix unused React import in App.tsx to pass strict build checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import { SavedRecipesProvider } from './SavedRecipesContext';
